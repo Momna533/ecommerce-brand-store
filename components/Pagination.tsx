571a370dc8317d49c2998691af1125512af2c9f9
Momna Ijaz <momna533gb@gmail.com>
2024-07-11 22:38:51 +0500

    Update Pagination.tsx

--- components/Pagination.tsx
+++ components/Pagination.tsx
@@ -1,23 +1,28 @@
 import { FaArrowLeft, FaArrowRight } from "react-icons/fa6";
 
-const Pagination = ({
-  currentPage,
-  setcurrentPage,
-  handlePageChange,
-  totalPages,
-  pages,
-}) => {
+const Pagination = ({ currentPage, handlePageChange, totalPages, pages }) => {
+  const scrollToTop = () => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  };
   return (
     <div className="pagination">
       {currentPage > 1 && (
-        <button onClick={() => handlePageChange(currentPage - 1)}>
+        <button
+          onClick={() => {
+            scrollToTop();
+            handlePageChange(currentPage - 1);
+          }}
+        >
           <FaArrowLeft />
         </button>
       )}
       {pages.map((page) => (
         <button
           key={page}
-          onClick={() => handlePageChange(page)}
+          onClick={() => {
+            scrollToTop();
+            handlePageChange(page);
+          }}
           style={{
             backgroundColor: currentPage === page ? "black" : "white",
             color: currentPage === page ? "white" : "black",
@@ -27,7 +32,12 @@ const Pagination = ({
         </button>
       ))}
       {currentPage < totalPages && (
-        <button onClick={() => handlePageChange(currentPage - 1)}>
+        <button
+          onClick={() => {
+            scrollToTop();
+            handlePageChange(currentPage - 1);
+          }}
+        >
           <FaArrowRight />
         </button>
       )}
